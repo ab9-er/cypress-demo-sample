0d6440e1775ed45794ff27ecee8d43905fc1efa2
Abhinav <abhinav.gamer@gmail.com>
2019-09-06 01:09:57 +0100

    fix indentation of spec file

--- cypress/integration/webtest/purchaseWorkflow.spec.js
+++ cypress/integration/webtest/purchaseWorkflow.spec.js
@@ -14,33 +14,33 @@ const checkoutPage = new checkoutPageObjects();
 
 describe('eCommerce search and buy', () => {
 
-    it('Given I navigate to the website', () => {
-      generic.openWebsite();
-      generic.clearPrivacyMessage();
-    });
-        
-    it('When I search for an iPhone to buy', () => {
-      homePage.searchProduct(Cypress.env('productName'));
-      generic.clearCookieMessage();
-      productPage.verifyProductLandingPage(Cypress.env('productName'));
-      productPage.selectProduct(Cypress.env('productId'));
-    });
+  it('Given I navigate to the website', () => {
+    generic.openWebsite();
+    generic.clearPrivacyMessage();
+  });
+      
+  it('When I search for an iPhone to buy', () => {
+    homePage.searchProduct(Cypress.env('productName'));
+    generic.clearCookieMessage();
+    productPage.verifyProductLandingPage(Cypress.env('productName'));
+    productPage.selectProduct(Cypress.env('productId'));
+  });
 
-    it('Then I should add the product to cart and proceed till checkout', () => {
-      productPage.addToBasket();
-      productPage.goToBasket();
-      generic.clearCookieMessage();
-      basketPage.continueToCheckout();
-      checkoutPage.selectGuestCheckoutMode();
-      checkoutPage.enterGuestEmailAndContinue(Cypress.env('guestEmail'));
-      checkoutPage.selectHomeDeliveryMode();
-      checkoutPage.fillGuestDetails(Cypress.env('guestTitle'), 
-                                    Cypress.env('guestFirstName'), 
-                                    Cypress.env('guestLastName'), 
-                                    Cypress.env('guestPhoneNumber'), 
-                                    Cypress.env('guestPostCode'));
-      checkoutPage.selectNamedDayDelivery();
-      checkoutPage.confirmDeliveryDetails();
-      generic.takeScreenshot(Cypress.env('screenshotFileName'));
+  it('Then I should add the product to cart and proceed till checkout', () => {
+    productPage.addToBasket();
+    productPage.goToBasket();
+    generic.clearCookieMessage();
+    basketPage.continueToCheckout();
+    checkoutPage.selectGuestCheckoutMode();
+    checkoutPage.enterGuestEmailAndContinue(Cypress.env('guestEmail'));
+    checkoutPage.selectHomeDeliveryMode();
+    checkoutPage.fillGuestDetails(Cypress.env('guestTitle'), 
+                                  Cypress.env('guestFirstName'), 
+                                  Cypress.env('guestLastName'), 
+                                  Cypress.env('guestPhoneNumber'), 
+                                  Cypress.env('guestPostCode'));
+    checkoutPage.selectNamedDayDelivery();
+    checkoutPage.confirmDeliveryDetails();
+    generic.takeScreenshot(Cypress.env('screenshotFileName'));
     });
 });
